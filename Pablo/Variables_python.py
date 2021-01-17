131c9e8f17281fb0221e9f2d3592638ee3723d06
Pablosanti77 <77211449+Pablosanti77@users.noreply.github.com>
2021-01-16 20:46:40 -0500

    Listo

---
+++ Pablo/Variables_python.py
@@ -0,0 +1,29 @@
+'''
+                CREACION DE VARIABLES
+
+INT = ENTEROS
+EJ: 123456789
+
+FLOAT = FLOTANTES
+12.3  5.3    69.4
+
+STRING = CADENA DE LETRAS O CARATERES
+'HOLA MUNDO' 'BAY '
+'''
+#Enteros
+i = 10
+b =int ( 10)
+#print(b)
+
+#Flotantes
+c = 5.3
+b = float ( 10.50)
+#print(b)
+
+#string
+c = 'hola '
+b = input("hola")
+#print(b)
+
+
+

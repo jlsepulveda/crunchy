330a5f8e8f9259e1752755d52ffc37408d4a04c8
Jose Sepulveda <jose.sepulveda@smiledirectclub.com>
2021-01-26 23:18:31 -0500

    Tuesday 26 Session.

---
+++ master/exersice2.py
@@ -0,0 +1,23 @@
+texto = "Hellowold"
+contador = 1
+texto_configurado = []
+
+print("Forma o solucion #1")
+for chart in texto:
+	if(contador%2==0):
+ 		texto_configurado.append(chart.upper())
+	else:
+		texto_configurado.append(chart.lower())
+	contador=contador+1
+
+print(str(texto_configurado))
+
+texto_configurado_2 = []
+print("Forma o solucion # 2")
+for i in range(1,len(texto)):
+	if(i%2==0):
+ 		texto_configurado_2.append(texto[i-1].upper())
+	else:
+		texto_configurado_2.append(texto[i-1].lower())
+
+print(str(texto_configurado_2))
